2d4026fc7a62e5c05ab87a296f53e13c6859bb90
DevelopersUsername <44446143+DevelopersUsername@users.noreply.github.com>
2020-04-26 21:06:40 +0300

    Init problem

---
+++ Problems/Chocolate/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+        int K = scanner.nextInt();
+
+        System.out.println(N * M >= K && (K % N == 0 || K % M == 0) ? "YES" : "NO");
+    }
+}
